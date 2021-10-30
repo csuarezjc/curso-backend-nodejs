17c2c4e6199d74e9b8f6774361005e1b8fb95a7f
Cristian Suarez <csuarezjc@gmail.com>
2021-10-30 15:54:50 -0500

    Separacion de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const faker = require('faker');
 const app = express();
 const port = 3000;
+
+const routeApi = require('./routes');
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -11,43 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.json([
-    { id, name: 'Product 1', price: 1000 },
-    { id, name: 'Product 2', price: 2000 },
-  ]);
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-
-  if (limit && offset) {
-    res.json({ limit, offset });
-  } else {
-    res.send('No hay parámetros');
-  }
-});
+routeApi(app);
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
