2774b4ce0f8bd22ecfef7537c10904ddc8941e83
Cristian Suarez <csuarezjc@gmail.com>
2021-10-30 15:55:11 -0500

    Separacion de responsabilidades con express.Router 2

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const productsRouter = require('./products');
+const usersRouter = require('./users');
+//const categoriesRouter = require('./categories');
+
+function routerApi(app) {
+  app.use('/products', productsRouter);
+  app.use('/users', usersRouter);
+  //app.use('/categories', categoriesRouter);
+}
+
+module.exports = routerApi;
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { limit, offset } = req.query;
+
+  if (limit && offset) {
+    res.json({ limit, offset });
+  } else {
+    res.json({});
+  }
+});
+
+module.exports = router;
