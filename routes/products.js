cf52afc98ef2503f0eb9cc6a93f033be9108010a
Cristian Suarez <csuarezjc@gmail.com>
2021-10-30 16:32:24 -0500

    PUT, PATCH y DELETE

--- routes/products.js
+++ routes/products.js
@@ -39,4 +39,24 @@ router.post('/', (req, res) => {
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router;
