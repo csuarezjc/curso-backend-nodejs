723bc2d648561c325879d563b09cff4cfa605adc
Cristian Suarez <csuarezjc@gmail.com>
2021-11-03 19:42:34 -0500

    Crear, editar y eliminar

--- routes/products.router.js
+++ routes/products.router.js
@@ -21,29 +21,33 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
+  const newProduct = service.create(body);
+
   res.status(201).json({
     message: 'created',
-    data: body,
+    data: newProduct,
   });
 });
 
 router.patch('/:id', (req, res) => {
   const body = req.body;
   const { id } = req.params;
+  const product = service.update(id, body);
 
   res.json({
     message: 'updated',
-    data: body,
+    data: product,
     id,
   });
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
+  const product = service.delete(id);
 
   res.json({
     message: 'deleted',
-    id,
+    product,
   });
 });
 
--- services/products.service.js
+++ services/products.service.js
@@ -17,15 +17,48 @@ class ProductsService {
       });
     }
   }
-  create() {}
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+
+    this.products.push(newProduct);
+
+    return newProduct;
+  }
   find() {
     return this.products;
   }
   findOne(id) {
     return this.products.find((item) => item.id === id);
   }
-  update() {}
-  delete() {}
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+
+    return this.products[index];
+  }
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+
+    this.products.splice(index, 1);
+
+    return { id };
+  }
 }
 
 module.exports = ProductsService;
