f2ed821d8ac383bd1891701a04d8d78e76e643eb
Cristian Suarez <csuarezjc@gmail.com>
2021-11-07 08:52:14 -0500

    Problema de CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routeApi = require('./routes');
 
 const {
@@ -11,6 +12,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
