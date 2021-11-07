518e20b635fc8a1681e58dfb8adfcd088a1a21b1
Cristian Suarez <csuarezjc@gmail.com>
2021-11-07 09:35:37 -0500

    Deployment a Heroku

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const whiteList = ['http://localhost:8080'];
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
