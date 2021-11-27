18e016635b6598ea081151e31960ce3557ba307b
Cristian Suarez <csuarezjc@gmail.com>
2021-11-27 15:24:06 -0500

    Que son las migraciones. Migraciones en Sequelize ORM

---
+++ db/config.js
@@ -0,0 +1,17 @@
+const { uriTooLong } = require('@hapi/boom');
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  },
+};
