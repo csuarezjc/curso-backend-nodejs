b3ff3e2cdfa2a91f71d5c4a31fd61abe7547ddc8
Cristian Suarez <csuarezjc@gmail.com>
2022-01-29 12:12:45 -0500

    Relaciones uno a uno

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 
 const productsRouter = require('./products.router');
-const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
+const customersRouter = require('./customers.router');
+const { custom } = require('joi');
 
 function routerApi(app) {
   const router = express.Router();
 
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
-  router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
